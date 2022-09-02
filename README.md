6543992f2bfcfac00da3e6d6b5cfd95e062ccd73
Hasan Riaz <hasanriaz2005@gmail.com>
2022-09-02 19:59:07 +0500

    updated app.js and readme files

---
+++ README.md
@@ -0,0 +1,4 @@
+## this is a header
+
+
+i am writing a paragraph;
--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const calculate = (a, b) => {
     return a + b;
 }
 
+const calculatePower = (a, b) => {
+    return a ** b;
+}
+
+const variable = "myVariable"
